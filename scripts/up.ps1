41ff0cb7ccef778c06a239326022e09c159ab6fc
HexagonalUniverse <hebertluizhd78@hotmail.com>
2026-06-16 11:41:39 -0300

    online DB testing...

--- scripts/up.ps1
+++ scripts/up.ps1
@@ -5,4 +5,4 @@
 Get-ChildItem
 
 docker build -t fsuy:ddr .
-docker run  --rm -p 4817:4817 fsuy:ddr
+docker run --rm -p 4817:4817 fsuy:ddr
--- src/backend/fsuy_site/models.py
+++ src/backend/fsuy_site/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+"""
+@file       backend/fsuy_site/urls.py
+@brief      ...
+@date       06-2026
+"""
 
-# Create your models here.
+from django.db import models
+from django.db.models import Field
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    uid: Field = models.BigIntegerField(
+        primary_key=True
+    )
+
+    socials: Field = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    steam: Field = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    discord: Field = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    last_login: Field = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    date: Field = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    is_admin: Field = models.BooleanField(
+        default=False
+    )
+
+
+class TesteTestado(models.Model):
+    coisa_legal: Field = models.CharField(max_length=255)
+
+
+
+print(
+    [
+        x.coisa_legal for x in TesteTestado.objects.all()
+    ]
+)
